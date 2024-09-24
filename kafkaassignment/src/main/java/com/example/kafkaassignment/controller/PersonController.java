da40a509e59c08ba5d6cc63016b9d298ce0b4f13
Harshana Gurusingha <harshanagurusingha@Harshanas-MacBook-Pro.local>
2024-09-24 22:02:44 +1000

    Added Logging

--- kafkaassignment/src/main/java/com/example/kafkaassignment/controller/PersonController.java
+++ kafkaassignment/src/main/java/com/example/kafkaassignment/controller/PersonController.java
@@ -3,6 +3,8 @@ package com.example.kafkaassignment.controller;
 
 import com.example.kafkaassignment.enums.KafkaTopics;
 import com.example.kafkaassignment.model.Person;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 public class PersonController {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
+    private static final Logger logger = LogManager.getLogger(PersonController.class);
 
     public PersonController(KafkaTemplate<String, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
@@ -20,6 +23,7 @@ public class PersonController {
     public String sendMessage(@RequestBody Person person) {
         String message = String.format("%s,%s,%s", person.firstName(), person.lastName(), person.dateOfBirth());
         kafkaTemplate.send(KafkaTopics.CUSTOMER_INPUT.getTopicName(), message);
+        logger.info("Message published successfully");
         return "Message sent to Kafka: " + message;
     }
 }
--- kafkaassignment/src/main/java/com/example/kafkaassignment/service/PersonMessageHandlerService.java
+++ kafkaassignment/src/main/java/com/example/kafkaassignment/service/PersonMessageHandlerService.java
@@ -1,7 +1,9 @@
 package com.example.kafkaassignment.service;
 
+import com.example.kafkaassignment.controller.PersonController;
 import com.example.kafkaassignment.enums.KafkaTopics;
-import com.example.kafkaassignment.model.Person;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,7 @@ import java.time.format.DateTimeFormatter;
 public class PersonMessageHandlerService {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
+    private static final Logger logger = LogManager.getLogger(PersonController.class);
 
     public PersonMessageHandlerService(KafkaTemplate<String, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
@@ -27,15 +30,8 @@ public class PersonMessageHandlerService {
         String dob = parts[2];
 
         int age = calculateAge(dob);
-        System.out.println("Consumed message: " + message + ", Calculated Age: " + age);
-
-        if (age % 2 == 0) {
-            kafkaTemplate.send(KafkaTopics.CUSTOMER_EVEN.getTopicName(), message);
-            System.out.println("Published to CustomerEVEN topic: " + message);
-        } else {
-            kafkaTemplate.send(KafkaTopics.CUSTOMER_ODD.getTopicName(), message);
-            System.out.println("Published to CustomerODD topic: " + message);
-        }
+        logger.info("Consumed message: " + message + ", Calculated Age: " + age);
+        publishMessageBasedOnPersonAge(age, message);
     }
 
     private int calculateAge(String dob) {
@@ -44,8 +40,14 @@ public class PersonMessageHandlerService {
         return Period.between(birthDate, LocalDate.now()).getYears();
     }
 
-    private void publishMessageBasedOnPersonAge(Person person) {
-
+    private void publishMessageBasedOnPersonAge(int age, String consumedMessage) {
+        if (age % 2 == 0) {
+            kafkaTemplate.send(KafkaTopics.CUSTOMER_EVEN.getTopicName(), consumedMessage);
+            logger.info("Published to CustomerEVEN topic: " + consumedMessage);
+        } else {
+            kafkaTemplate.send(KafkaTopics.CUSTOMER_ODD.getTopicName(), consumedMessage);
+            logger.info("Published to CustomerODD topic: " + consumedMessage);
+        }
     }
 }
 
